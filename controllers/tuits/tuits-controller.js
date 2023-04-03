828f739a557e281344d6a4e3e853a344a432bcbb
shajiehhh <huang.shaoj@northeastern.edu>
2023-04-03 12:36:39 -0700

    fix connection error

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,16 +1,16 @@
 import * as tuitsDao from '../tuits/tuits-dao.js'
 
-const createTuit = async (req, res) => {
+const createTuit = async(req, res) => {
     const newTuit = req.body;
     newTuit.likes = 0;
     newTuit.liked = false;
-    const insertedTuit = await tuitsDao
-        .createTuit(newTuit);
+    newTuit.dislikes = 0;
+    newTuit.disliked = false;
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
     res.json(insertedTuit);
 }
 
-
-const findTuits = async (req, res) => {
+const findTuits  = async (req, res) => {
     const tuits = await tuitsDao.findTuits()
     res.json(tuits);
 }
@@ -18,17 +18,24 @@ const findTuits = async (req, res) => {
 const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
+//  const tuitIndex = tuits.findIndex(
+//    (t) => t._id === tuitdIdToUpdate)
+//  tuits[tuitIndex] =
+//    {...tuits[tuitIndex], ...updates};
     const status = await tuitsDao
         .updateTuit(tuitdIdToUpdate,
             updates);
     res.json(status);
 }
 
-
-const deleteTuit = async (req, res) => {
+const deleteTuit = async(req, res) => {
+    // convert string to number
     const tuitdIdToDelete = req.params.tid;
+//  tuits = tuits.filter((t) =>
+//    t._id !== tuitdIdToDelete);
     const status = await tuitsDao
         .deleteTuit(tuitdIdToDelete);
+    //res.send(`Deleting tuit ${tuitdIdToDelete}`);
     res.json(status);
 }
 
--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
     tuit: String,
+    image: String,
+    topic: String,
+    title: String,
+    handle: String,
     likes: Number,
+    dislikes: Number,
     liked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
