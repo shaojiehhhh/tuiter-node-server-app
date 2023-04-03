3709598cdee7de8705092266fd9f68a36b1a0e4f
shajiehhh <huang.shaoj@northeastern.edu>
2023-04-03 11:11:30 -0700

    A9 mongoDB

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb+srv://Cluster37891:8l08UGOIvb7b7QV1@cluster37891.kn3tfwt.mongodb.net/?retryWrites=true&w=majority');
+
 const app = express()
 app.use(cors())
 app.use(express.json());
