8ad2ec146042b830af880e53582f829cf4fe3dcf
shajiehhh <huang.shaoj@northeastern.edu>
2023-04-03 11:57:56 -0700

    fix connection error

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb+srv://Cluster37891:8l08UGOIvb7b7QV1@cluster37891.kn3tfwt.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter'
+    || 'mongodb://127.0.0.1:27017/tuiter');
+// mongoose.connect('mongodb+srv://Cluster37891:8l08UGOIvb7b7QV1@cluster37891.kn3tfwt.mongodb.net/?retryWrites=true&w=majority');
 
 const app = express()
 app.use(cors())
